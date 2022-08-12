f50390e7e96df337ff16437646768ee6588a6a8b
Azhar Khan <admin@azhark.com>
2022-08-12 09:36:12 +0530

    switch basic

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/**
+ * 
+ * values accepted are only int, char, enum
+ * 
+**/ 
+
+
+int main() {
+
+
+    int num =5;
+
+    switch (num) {
+        case 1: cout << " Hello number 1 " << endl;
+                break;
+        case 2: cout << "Hello number 2 " <<endl;
+                break;
+        case 3: cout << "Hello number 2 " <<endl;
+                break;
+        default: cout << " This is default number " << endl;
+    }
+
+}
