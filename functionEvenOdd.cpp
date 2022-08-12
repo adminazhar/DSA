d7f00895125d3c11c23d6d2e2f1cb8f4ba5ffa6e
Azhar Khan <admin@azhark.com>
2022-08-12 10:02:34 +0530

    check even odd using function

---
+++ functionEvenOdd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int a) {
+    if(a&1) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+
+int main() {
+    int num;
+    cout << "please enter number: " << endl;
+    cin >> num;
+
+    if(isEven(num)) {
+        cout << "Number is even" << endl;
+    } else {
+        cout << "Number is odd" << endl;
+    }
+
+
+}
