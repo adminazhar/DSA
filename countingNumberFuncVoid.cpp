be2a35b677e316376a1b25704e405c386f204bf7
Azhar Khan <admin@azhark.com>
2022-08-15 00:09:26 +0530

    counting number

---
+++ countingNumberFuncVoid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @author Azhar Khan
+ * @email admin@azhark.com
+ * Counting programing with function with void
+*/
+
+countNumber(n){
+    
+}
+
+int main(){
+    int n;
+    cout << "Please enter value of N: " << endl;
+    cin >> n;
+
+    countNumber(n);
+    return 0;
+}
