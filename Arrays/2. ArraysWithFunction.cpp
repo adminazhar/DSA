3b82faf617e28da885e601cf460a954b2ae1786a
Azhar Khan <admin@azhark.com>
2022-08-15 01:37:03 +0530

    Using arrays in functions

--- Arrays/2. ArraysWithFunction.cpp
+++ Arrays/2. ArraysWithFunction.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
-
+/**
+ * @adminazhar
+ * Using arrays in functions
+ * we can use int size = sizeof(arr)/sizeof(int) for calculating size,
+ *  but it will consider empty/garbage elements too.
+*/
 
 void printArray(int arr[], int size) {
+    cout << "---PRINT START---" <<endl;
+    for(int i=0;i<size;i++){
+        cout << "Value at: " << i << " is: " << arr[i] << endl;
+    }
+    cout << "---PRINT END---" <<endl;
+}
+
+void printArrayChar(char arr[], int size) {
     cout << "---PRINT START---" <<endl;
     for(int i=0;i<size;i++){
         cout << "Value at: " << i << " is: " << arr[i] << endl;
@@ -19,6 +32,8 @@ int main() {
     int number[n] = {0};
     printArray(number, n);
 
+    char gotham[5] = {'g', 'o', 't', 'h', 'a'};
+    printArrayChar(gotham, 5);
 
 
 }
