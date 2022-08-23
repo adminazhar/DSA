f81939381247489528442bfa82d34f4e6b4cf29c
Azhar Khan <admin@azhark.com>
2022-08-23 23:22:13 +0530

    Print sum of all elements in an array

--- Arrays/6. Array Sum.cpp
+++ Arrays/6. Array Sum.cpp
@@ -6,8 +6,26 @@ using namespace std;
  */
 
 
+int SumArr(int arr[], int size) {
+    int sum = 0;
+    for (int i=0; i<size; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
 
 
 int main() {
-    
+    int size;
+    cout << "Enter the array size: " << endl;
+    cin >> size;
+
+    int arr[100];
+
+    for (int i=0; i<size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "The sum of all values in array is: " << SumArr(arr, size) << endl;
+
 }
