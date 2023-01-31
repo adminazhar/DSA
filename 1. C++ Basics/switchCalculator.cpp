c0aecd2eee391349e610fff597fa6252f5c0a4a9
Azhar Khan <admin@azhark.com>
2023-01-31 09:43:02 +0530

    Switch Calculator

--- 1. C++ Basics/switchCalculator.cpp
+++ 1. C++ Basics/switchCalculator.cpp
@@ -1,33 +1,35 @@
+/**
+ * Calculator using Switch
+*/
+
 #include <iostream>
 using namespace std;
 
 
 int main (){
     int num1,num2;
-    cout << "Please enter number 1: " << endl;
-    cin >> num1;
-    cout << "Please enter number 2: " << endl;
-    cin >> num2;
-
     char op;
-    cout << "Please enter operation: + - / *" <<endl;
+    cout << "Enter First Number: " << endl;
+    cin >> num1;
+    cout << "Enter Second Number: " << endl;
+    cin >> num2;
+    cout << "Enter the operation: " << endl;
     cin >> op;
 
-    switch (op) {
-        case '+' : 
-            cout  << "Addition is : " << (num1+num2) << endl;
+    switch(op) {
+        case '+':
+            cout << "The sum is: " << (num1+num2) << endl;
             break;
-        case '-' : 
-            cout  << "Substraction is : " << (num1-num2) << endl;
+        case '-':
+            cout << "The Minus is: " << (num2-num1) << endl;
             break;
-        case '/' : 
-            cout  << "Division is : " << (num1/num2) << endl;
+        case '*':
+            cout << "The Multiply is: " << (num2*num1) << endl;
             break;
-        case '*' : 
-            cout  << "Multiplication is : " << (num1+num2) << endl;
+        case '/':
+            cout << "The Divide is: " << (num2/num1) << endl;
             break;
         default:
-            cout << "Please enter operation" << endl;
-            break;
+            cout << "No operation provided";
     }
 }
